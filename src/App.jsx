bd9b96847960121acff28408e6189d837819fb4a
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-02-10 15:58:25 +0800

    feat: add router config and Link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Link } from 'react-router';
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <nav>
         <ul>
           <li>
-            <a href="profile">Profile page</a>
+            <Link to="profile">Profile page</Link>{' '}
           </li>
         </ul>
       </nav>
