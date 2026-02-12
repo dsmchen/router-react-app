0028cc63fc2f85f7695be3e076a26141413fd694
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-02-12 13:23:23 +0800

    feat: create and render browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>Hello!</h1>
+    <RouterProvider router={router} />{' '}
   </StrictMode>,
 );
