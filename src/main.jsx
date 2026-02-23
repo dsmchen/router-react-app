9d13de23fdab9b44b99e468ec1e913889d9f68a7
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-02-24 06:23:41 +0800

    feat: update contacts with FormData

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Root, {
   action as rootAction,
 } from './routes/root';
 import Contact, { loader as contactLoader } from './routes/contact';
-import EditContact from './routes/edit';
+import EditContact, { action as editAction } from './routes/edit';
 
 const router = createBrowserRouter([
   {
@@ -27,6 +27,7 @@ const router = createBrowserRouter([
         path: 'contacts/:contactId/edit',
         element: <EditContact />,
         loader: contactLoader,
+        action: editAction,
       },
     ],
   },
