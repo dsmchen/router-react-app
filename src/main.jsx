83e10b58fa67437b9f7f1bad2b6bfb5f5c004356
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-02-23 22:26:24 +0800

    feat: update data

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Root, {
   action as rootAction,
 } from './routes/root';
 import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact from './routes/edit';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
       {
         path: 'contacts/:contactId',
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
